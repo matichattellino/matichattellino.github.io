02af155fa5bcd4ecd45e7e94ffc6466a813fc6d5
matias chattellino <matiaschattellino@gmail.com>
2019-10-30 17:14:09 -0300

    primer commit

---
+++ _next/static/webpack/static/development/pages/index.js.1538d75d4c64e3c7ee4a.hot-update.js
@@ -0,0 +1,72 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/Evento.js":
+/*!******************************!*\
+  !*** ./components/Evento.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Users\\matia\\Desktop\\bitcoinApp\\components\\Evento.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var Evento = function Evento(props) {
+  var _props$info = props.info,
+      name = _props$info.name,
+      url = _props$info.url,
+      description = _props$info.description;
+  var titulo = name.text;
+
+  if (titulo.leng > 250) {
+    titulo = titulo.substr(0, 100);
+  }
+
+  var descripcion = description.text;
+
+  if (descripcion) {
+    desc = desc.substr(0, 250);
+  }
+
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, __jsx("a", {
+    href: url,
+    target: "_blank",
+    className: "list-group-item active text-light mb-1",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, __jsx("h3", {
+    className: "mb-3",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  }, titulo), __jsx("p", {
+    className: "mb-1",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    },
+    __self: this
+  }, descripcion)));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Evento);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.1538d75d4c64e3c7ee4a.hot-update.js.map
